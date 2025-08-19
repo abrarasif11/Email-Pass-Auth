d6d742bd2c9c750b5339e2339e86a7dbb944fcee
abrarasif11 <faasif929@gmail.com>
2025-08-19 22:21:57 +0600

    on-submit

--- src/component/Register/Register.jsx
+++ src/component/Register/Register.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const Register = () => {
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(event.target.email.value)
+  }
   return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <div className="max-w-lg mx-auto mt-20">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <div className="card-body">
+              <fieldset className="fieldset">
+                <label className="label">Email</label>
+                <input type="email" name="email" className="input" placeholder="Email" />
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                />
+                <div>
+                  <a className="link link-hover">Forgot password?</a>
+                </div>
+                <button className="btn btn-neutral mt-4">Login</button>
+              </fieldset>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+};
 
-export default Register
+export default Register;
