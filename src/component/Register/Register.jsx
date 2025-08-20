e79d9db81224a64c22b6b57b5bd2da8bfd79fa12
abrarasif11 <faasif929@gmail.com>
2025-08-20 11:18:03 +0600

    find-email-pass-value-on-console

--- src/component/Register/Register.jsx
+++ src/component/Register/Register.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 
 const Register = () => {
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(event.target.email.value)
-  }
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log(email,password)
+  };
   return (
     <form onSubmit={handleSubmit}>
       <div className="max-w-lg mx-auto mt-20">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-          </div>
+          <div className="text-center lg:text-left"></div>
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
             <div className="card-body">
               <fieldset className="fieldset">
                 <label className="label">Email</label>
-                <input type="email" name="email" className="input" placeholder="Email" />
+                <input
+                  type="email"
+                  name="email"
+                  className="input"
+                  placeholder="Email"
+                />
                 <label className="label">Password</label>
                 <input
                   type="password"
                   className="input"
+                  name="password"
                   placeholder="Password"
                 />
                 <div>
